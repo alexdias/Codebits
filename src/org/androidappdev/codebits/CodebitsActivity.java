6d8c27e5b020263bbf6ee7fcc6dc159a648207a2
Henrique Rocha <hmrocha@gmail.com>
2011-08-21 23:46:14 +0100

    List talk titles.

--- src/org/androidappdev/codebits/CodebitsActivity.java
+++ src/org/androidappdev/codebits/CodebitsActivity.java
@@ -18,14 +18,68 @@
 
 package org.androidappdev.codebits;
 
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class CodebitsActivity extends Activity {
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+	private static String TAG = "CODEBITS";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		StringBuilder builder = new StringBuilder();
+		HttpClient client = new DefaultHttpClient();
+		HttpGet request = new HttpGet(
+				"https://services.sapo.pt/Codebits/calltalks");
+		try {
+			HttpResponse response = client.execute(request);
+			HttpEntity entity = response.getEntity();
+			InputStream content = entity.getContent();
+			BufferedReader reader = new BufferedReader(new InputStreamReader(
+					content));
+			String line = null;
+			while ((line = reader.readLine()) != null) {
+				builder.append(line);
+			}
+		} catch (Exception e) {
+			Log.d(TAG, "EXCEPTION", e);
+		}
+
+		String[] talkTitles = null;
+		try {
+			String text = builder.toString();
+			builder = new StringBuilder();
+			JSONArray jsonArray = new JSONArray(text);
+			talkTitles = new String[jsonArray.length()];
+			for (int i = 0; i < jsonArray.length(); i++) {
+				JSONObject jsonObject = jsonArray.getJSONObject(i);
+				talkTitles[i] = jsonObject.getString("title");
+			}
+		} catch (JSONException e) {
+			Log.d(TAG, "EXCEPTION", e);
+		}
+
+		ListView listView = (ListView) findViewById(R.id.talks);
+		listView.setAdapter(new ArrayAdapter<String>(this,
+				android.R.layout.simple_list_item_1, talkTitles));
+	}
 }
